34b64f48d60ea55a7a4181d5f355dbe5eacf5051
Kazamaki76 <140473875+Kazamaki76@users.noreply.github.com>
2024-06-13 16:29:37 +0700

    add nav to page

--- codemobile/src/components/navbar.tsx
+++ codemobile/src/components/navbar.tsx
@@ -1,10 +1,21 @@
-import {Container, Navbar as NavbarBs} from 'react-bootstrap'
+import {Button, Container, Nav, Navbar as NavbarBs} from 'react-bootstrap'
+import { NavLink } from 'react-router-dom'
 
 export function Navbar() {
     return (
         <NavbarBs className="bg-white shadow-sm mb-3">
             <Container>
-                Nav
+                <Nav className='me-auto'>
+                    <Nav.Link to ="/" as={NavLink}  >
+                    Home
+                    </Nav.Link>
+                    <Nav.Link to ="/detail" as={NavLink}  >
+                    detail
+                    </Nav.Link>
+                    <Nav.Link to ="/summary" as={NavLink}  >
+                    summary
+                    </Nav.Link>
+                </Nav>
             </Container>
         </NavbarBs>
     )
