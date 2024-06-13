f6af714c05e68335963ed81212a0c5460ef720f4
Kazamaki76 <140473875+Kazamaki76@users.noreply.github.com>
2024-06-13 15:35:38 +0700

    add route

--- codemobile/src/App.tsx
+++ codemobile/src/App.tsx
@@ -1,8 +1,21 @@
+import {Routes, Route} from 'react-router-dom'
+import { Container } from 'react-bootstrap'
+import { Home } from './pages/HomePage'
+import { Login } from './pages/LoginPage'
+import { Detail } from './pages/DetailPage'
+import { Summary } from './pages/SummaryPage'
 function App() {
 
 
   return (
-    <h1> Yolo</h1>
+    <Container>
+      <Routes>
+        <Route path="/" element={<Login/>} />
+        <Route path="/home" element={ <Home/>} />
+        <Route path="/detail" element={ <Detail/>} />
+        <Route path="/summary" element={ <Summary/>} />
+      </Routes>
+    </Container>
   )
 }
 
